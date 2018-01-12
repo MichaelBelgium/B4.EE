13b1bd4e0d62c7936a154d94a4dc9d6892115afa
MichaelBelgium <michaelvrld@gmail.com>
2018-01-12 14:41:13 +0100

    First test

--- B4.EE.MV/B4.EE.MV/ViewModels/MainViewModel.cs
+++ B4.EE.MV/B4.EE.MV/ViewModels/MainViewModel.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using System.Windows.Input;
+using B4.EE.MV.Services;
 using FreshMvvm;
+using Xamarin.Forms;
 
 namespace B4.EE.MV.ViewModels
 {
     class MainViewModel: FreshBasePageModel
     {
-        
+        private ApiService service;
+
+        public MainViewModel()
+        {
+            service = new ApiService();
+        }
+
+        public ICommand GetRequest => new Command(
+            () => service.GetCityWeather("Brugge")
+        );
     }
 }
