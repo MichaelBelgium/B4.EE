e2df9c870734349e852bf9e99dce0f6cb9bed7d5
MichaelBelgium <michaelvrld@gmail.com>
2018-01-12 14:39:04 +0100

    Create ApiResponse model and ApiService

---
+++ B4.EE.MV/B4.EE.MV/Models/WeatherApiResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B4.EE.MV.Models
+{
+    public class WeatherApiResponse
+    {
+        public string Name { get; set; }
+        public Position Coord { get; set; }
+    }
+
+    public class Position
+    {
+        public float Lon { get; set; }
+        public float Lat { get; set; }
+    }
+}
---
+++ B4.EE.MV/B4.EE.MV/Services/ApiService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using RestSharp;
+using B4.EE.MV.Models;
+
+namespace B4.EE.MV.Services
+{
+    class ApiService
+    {
+        private const string API_KEY = "30d1c831738ed536e2e15616249e2570";
+        private const string API_URL = "http://api.openweathermap.org";
+        private RestClient client;
+
+        public ApiService()
+        {
+            client = new RestClient(API_URL);
+        }
+
+        public WeatherApiResponse GetCityWeather(string city)
+        {
+            string json = string.Empty;
+            var request = new RestRequest("data/2.5/weather",Method.GET);
+
+            request.AddParameter("q", city);
+            request.AddParameter("APPID", API_KEY);
+            request.AddParameter("units", "metric");
+
+            IRestResponse<WeatherApiResponse> response = client.Execute<WeatherApiResponse>(request);
+
+            return response.Data;
+        }
+    }
+}
