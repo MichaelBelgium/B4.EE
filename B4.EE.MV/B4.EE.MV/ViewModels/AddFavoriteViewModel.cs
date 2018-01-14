c828be34f5049a25644bfa246bf1e99ba3e7e127
MichaelBelgium <michaelvrld@gmail.com>
2018-01-14 19:59:57 +0100

    Add ability to add favorite location by city

---
+++ B4.EE.MV/B4.EE.MV/ViewModels/AddFavoriteViewModel.cs
@@ -0,0 +1,49 @@
+﻿using B4.EE.MV.Services;
+using FreshMvvm;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace B4.EE.MV.ViewModels
+{
+    class AddFavoriteViewModel: FreshBasePageModel
+    {
+        private string errorMessage, city;
+        private ApiService service;
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+            service = new ApiService();
+        }
+
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { errorMessage = value; RaisePropertyChanged(nameof(ErrorMessage)); }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; RaisePropertyChanged(nameof(City)); }
+        }
+
+
+        public ICommand AddCity => new Command(
+            async () =>
+            {
+                if (string.IsNullOrEmpty(City))
+                {
+                    ErrorMessage = "Enter a city";
+                    return;
+                }
+                
+                var tmp = await service.GetCityWeather(City);
+                if (tmp == null)
+                    ErrorMessage = "City not found";
+                else
+                    await CoreMethods.PopPageModel(tmp);
+            }
+        );
+    }
+}
--- B4.EE.MV/B4.EE.MV/ViewModels/FavoritesViewModel.cs
+++ B4.EE.MV/B4.EE.MV/ViewModels/FavoritesViewModel.cs
@@ -1,11 +1,20 @@
-﻿using FreshMvvm;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using B4.EE.MV.Models;
+using FreshMvvm;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace B4.EE.MV.ViewModels
 {
-    class FavoritesViewModel: FreshBasePageModel
+    public class FavoritesViewModel: FreshBasePageModel
     {
+        public ICommand AddFav => new Command(
+            async () => await CoreMethods.PushPageModel<AddFavoriteViewModel>()
+        );
+
+        public override void ReverseInit(object returnedData)
+        {
+            WeatherApiResponse newFavorite = returnedData as WeatherApiResponse;
+            //todo process more
+        }
     }
 }
