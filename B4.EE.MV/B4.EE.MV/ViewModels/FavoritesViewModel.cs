ec1098df3e900b191bcbceb8a5d99a6f99876ade
MichaelBelgium <michaelvrld@gmail.com>
2018-01-15 20:00:54 +0100

    Add ability to add locations to favorites

--- B4.EE.MV/B4.EE.MV/ViewModels/FavoritesViewModel.cs
+++ B4.EE.MV/B4.EE.MV/ViewModels/FavoritesViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using B4.EE.MV.Models;
+using B4.EE.MV.Models.Database;
+using B4.EE.MV.Services;
 using FreshMvvm;
+using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -7,14 +11,37 @@ namespace B4.EE.MV.ViewModels
 {
     public class FavoritesViewModel: FreshBasePageModel
     {
+        private DatabaseService dbService;
+        private ObservableCollection<Location> locationList;
+        
+        public override void Init(object initData)
+        {
+            dbService = new DatabaseService();
+        }
+
         public ICommand AddFav => new Command(
             async () => await CoreMethods.PushPageModel<AddFavoriteViewModel>()
         );
 
+        public ICommand Refresh => new Command(
+            () => LocationList = new ObservableCollection<Location>(dbService.GetLocations())
+        );
+
+        public ObservableCollection<Location> LocationList
+        {
+            get { return locationList; }
+            set { locationList = value; RaisePropertyChanged(nameof(LocationList)); }
+        }
+
         public override void ReverseInit(object returnedData)
         {
             WeatherApiResponse newFavorite = returnedData as WeatherApiResponse;
-            //todo process more
+            dbService.AddLocation(newFavorite.Id, newFavorite.Name);
+        }
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            Refresh.Execute(null);
         }
     }
 }
