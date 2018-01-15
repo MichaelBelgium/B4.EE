3b8f8893b185a365c78c7732327949016a6b60cf
MichaelBelgium <michaelvrld@gmail.com>
2018-01-15 20:54:46 +0100

    Add 2 more pages/models + complete favoritespage (for now)

---
+++ B4.EE.MV/B4.EE.MV/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,12 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B4.EE.MV.ViewModels
+{
+    class WeatherViewModel: FreshBasePageModel
+    {
+
+    }
+}
