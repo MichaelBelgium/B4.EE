3a9762bd2f2853d1e8fec784e6fa80f7362e25c8
MichaelBelgium <michaelvrld@gmail.com>
2018-01-16 20:21:32 +0100

    Start of WeatherPage + add color to error message

--- B4.EE.MV/B4.EE.MV/ViewModels/WeatherViewModel.cs
+++ B4.EE.MV/B4.EE.MV/ViewModels/WeatherViewModel.cs
@@ -1,12 +1,32 @@
-﻿using FreshMvvm;
+﻿using B4.EE.MV.Models;
+using B4.EE.MV.Models.Database;
+using B4.EE.MV.Services;
+using FreshMvvm;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace B4.EE.MV.ViewModels
 {
     class WeatherViewModel: FreshBasePageModel
     {
+        private WeatherApiResponse currentResponse;
+        private ApiService service;
 
+        public override void Init(object initData)
+        {
+            service = new ApiService();
+            Refresh.Execute(null);
+        }
+
+        public ICommand Refresh => new Command(
+            async () => CurrentResponse = await service.GetCityWeather(Settings.SelectedLocation.Name)
+        );
+
+        public WeatherApiResponse CurrentResponse
+        {
+            get { return currentResponse; }
+            set { currentResponse = value; RaisePropertyChanged(nameof(CurrentResponse)); }
+        }
     }
 }
