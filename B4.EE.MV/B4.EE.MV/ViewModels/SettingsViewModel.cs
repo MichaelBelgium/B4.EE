9ddfa79f0f5f09f185766805eaeefc1a9175de80
MichaelBelgium <michaelvrld@gmail.com>
2018-01-17 20:09:30 +0100

    Add first setting and abiity to save

--- B4.EE.MV/B4.EE.MV/ViewModels/SettingsViewModel.cs
+++ B4.EE.MV/B4.EE.MV/ViewModels/SettingsViewModel.cs
@@ -1,12 +1,29 @@
-﻿using FreshMvvm;
-using System;
+﻿using B4.EE.MV.Models.Database;
+using FreshMvvm;
 using System.Collections.Generic;
-using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace B4.EE.MV.ViewModels
 {
     class SettingsViewModel: FreshBasePageModel
     {
-       
+        public List<string> UnitItems
+        {
+            get { return new List<string> { Settings.UNIT_IMPERIAL, Settings.UNIT_METRIC, Settings.UNIT_STANDARD }; }
+        }
+        private string selectedUnit;
+
+        public ICommand SaveSettings => new Command(async () => {
+            Settings.Unit = SelectedUnit;
+            await CoreMethods.DisplayAlert("Success", "Settings were saved.", "Ok");
+            await CoreMethods.PopToRoot(false);
+        });
+
+        public string SelectedUnit
+        {
+            get { return selectedUnit; }
+            set { selectedUnit = value; }
+        }
     }
 }
